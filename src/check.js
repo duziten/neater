bdf6371b90a5d461916b949325cb0123e8377417
dusongzhan <dusongzhan4422@haixue.com>
2020-09-04 09:47:21 +0800

    fix: fix check

--- src/check.js
+++ src/check.js
@@ -3,17 +3,16 @@ const getGitBranchs = require('../utils').getGitBranchs;
 
 module.exports = function (config = {}) {
   const { number, ignore } = config;
-  getGitBranchs((branchs) => {
-    if (branchs.length <= Number(number)) {
-      console.log(chalk.green('Ooo~ You are a neater!'));
-    } else {
-      console.log(
-        chalk.white(
-          `Something need to clear, run ${chalk.green(
-            'nt clear [options]'
-          )} can help!`
-        )
-      );
-    }
-  }, ignore || []);
+  const branchs = getGitBranchs(ignore || []);
+  if (branchs.length <= Number(number)) {
+    console.log(chalk.green('Ooo~ You are a neater!'));
+  } else {
+    console.log(
+      chalk.white(
+        `Something need to clear, run ${chalk.green(
+          'nt clear [options]'
+        )} can help!`
+      )
+    );
+  }
 };
